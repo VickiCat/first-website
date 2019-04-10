91ea26edbca38a023627d86a57133926d6267837
Vicki Semchuk <victoriasemchuk@gmail.com>
2019-04-10 15:48:08 +0000

    demo header

--- js/index.js
+++ js/index.js
@@ -13,3 +13,25 @@ function updateClock ()
 
   document.getElementById("clock").firstChild.nodeValue = currentTimeString;
 }
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+
+
+document.querySelector('.fetch-html')
+    .addEventListener('click', fetchHtml);
+
+function fetchHtml() {
+    fetch('thanks.html')
+        .then( response => response.text() )
+        .then( html => document.querySelector('.html-placeholder').innerHTML = html)
+        .catch( error => document.querySelector('.html-placeholder').innerHTML = error.message );
+}
+
